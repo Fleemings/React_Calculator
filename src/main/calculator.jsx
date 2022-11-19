fdaf6b06e1e52358aa153960ccb0b971968b4161
Milena Fleming <milenasapucaia29@gmail.com>
2022-11-19 21:41:40 +0000

    feat: adding style to the main index page
    style set and also added the main page calculator to index

---
+++ src/main/calculator.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './calculator.css';
+import Button from '../components/button/button';
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div className='calculator'>
+        <Button label='AC' />
+        <Button label='/' />
+        <Button label='7' />
+        <Button label='8' />
+        <Button label='9' />
+        <Button label='*' />
+        <Button label='4' />
+        <Button label='5' />
+        <Button label='6' />
+        <Button label='-' />
+        <Button label='1' />
+        <Button label='2' />
+        <Button label='3' />
+        <Button label='+' />
+        <Button label='0' />
+        <Button label='.' />
+        <Button label='=' />
+      </div>
+    );
+  }
+}
+
+export default Calculator;
